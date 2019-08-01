a62f998c036d22f684259a9d36bda64a0b658a2c
Ali Ismael <aliismaelabdelwahab@gmail.com>
2019-08-01 12:28:20 +0200

    Update ticketing-widget.js

--- ticketing-widget.js
+++ ticketing-widget.js
@@ -26,7 +26,7 @@ function eventtusWidget() {
       iframe.id = 'eventtusWidgetFrame';
       iframe.src = domainURL + '/' + eventId + '/tickets?mode=embed';
       iframe.style =
-        `position: fixed; top: 0px; left: 0px; display: block; margin: 0px; padding: 0px; border: none; height: 100vh; width: 100vw; z-index: 2147483647; background-color: rgba(56, 64, 74, 0.9); transition: background-color 1s cubic-bezier(0.645, 0.045, 0.355, 1)`;
+        `position: absolute; top: 0px; left: 0px; display: block; margin: 0px; padding: 0px; border: none; height: 100vh; width: 100vw; z-index: 2147483647; background-color: rgba(56, 64, 74, 0.9); transition: background-color 1s cubic-bezier(0.645, 0.045, 0.355, 1)`;
       document.body.appendChild(iframe);
     } catch (err) {
       window.open(domainURL + '/' + eventId + '/tickets', '_blank');
