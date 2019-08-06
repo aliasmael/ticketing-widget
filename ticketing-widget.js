04b6f9142929e081eaae32582dd02ba0ccc6fd03
Ali Ismael <aliismaelabdelwahab@gmail.com>
2019-08-06 14:37:57 +0200

    Update ticketing-widget.js

--- ticketing-widget.js
+++ ticketing-widget.js
@@ -40,7 +40,7 @@ function eventtusWidget() {
       iframe.id = 'eventtusWidgetFrame';
       iframe.src = domainURL + '/' + eventId + '/tickets?mode=embed';
       iframe.style =
-        `position: absolute; top: 0px; left: 0px; display: block; margin: 0px; padding: 0px; border: none; height: 100vh; width: 100vw; z-index: 2147483647; background-color: rgba(56, 64, 74, 0.9); transition: background-color 1s cubic-bezier(0.645, 0.045, 0.355, 1)`;
+        `position: fixed; top: 0px; left: 0px; display: block; margin: 0px; padding: 0px; border: none; height: 100vh; width: 100vw; z-index: 2147483647; background-color: rgba(56, 64, 74, 0.9); transition: background-color 1s cubic-bezier(0.645, 0.045, 0.355, 1)`;
       document.body.appendChild(iframe);
 
       // Fix scrolling issue with iframe
@@ -48,6 +48,7 @@ function eventtusWidget() {
       document.body.style.height = "auto";
       
       if (iOS) {
+        iframe.style.position = 'relative';
         document.querySelectorAll('body > *').forEach(function (elm) { elm.style.display = 'none' });
         document.getElementById('eventtusWidgetFrame').style.display = 'block';
       }
